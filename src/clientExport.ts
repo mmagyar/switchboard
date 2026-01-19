4ab1d81c3cc807665200abc8d0f3966a47f8dc8b
Magyar Máté <dev@mmagyar.com>
2026-01-19 13:08:09 +0100

    Update clientExport.ts

--- src/clientExport.ts
+++ src/clientExport.ts
@@ -1,3 +1,4 @@
 export { defToUrl } from "./urlUtils.ts";
 export { call, setBaseUrl } from "./clientApiCall.ts";
 export { define } from "./routeDef.ts";
+export { parseUrl } from "./routeParser.ts";
